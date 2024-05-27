0057e6c72ef48e479ef94b632df8f9248dd12a58
garyung <garung@dekina.com>
2024-05-27 21:44:06 +0900

    refresh 실행, refreah_time 일정 시간 토큰 부여

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,25 @@
 <script>
+  import {onMount} from 'svelte';
+  import {auth, isRefresh} from "./stores";
   import Router from "./router.svelte";
+
+  const refresh_time = 1000 * 60 * 14
+
+  onMount(() => {
+    const onRefresh = setInterval(() => {
+      if($isRefresh) {
+        auth.refresh()
+      }else {
+        clearInterval(onRefresh)
+      }
+    }, refresh_time)  
+  })
 </script>
 
 
 <div class="wrap">
-  <Router />
+  {#await auth.refresh() then}
+    <Router />
+  {/await}
 </div>
 
